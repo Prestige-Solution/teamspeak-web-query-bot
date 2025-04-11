c7cccb3bb6f69ca63b3e6c7ce01df10b2aca924f
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-11 10:17:43 +0200

    add new free fonts

--- database/seeders/catFontSeeder.php
+++ database/seeders/catFontSeeder.php
@@ -13,7 +13,7 @@ class catFontSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         catFont::query()->create([
             'name'=>'Arial',
@@ -28,6 +28,36 @@ class catFontSeeder extends Seeder
         catFont::query()->create([
             'name'=>'Univers Bold Italic',
             'font_name'=>'Univers_LT_66_Bold_Italic.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'Baroque Script',
+            'font_name'=>'BaroqueScript.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'Broadcast Titling',
+            'font_name'=>'BroadcastTitling.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'Canterbury',
+            'font_name'=>'Canterbury.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'OldEnglish Regular',
+            'font_name'=>'OldEnglishRegular.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'Rothenburg Decorative',
+            'font_name'=>'RothenburgDecorative.ttf',
+        ]);
+
+        catFont::query()->create([
+            'name'=>'Transformers Movie',
+            'font_name'=>'TransformersMovie.ttf',
         ]);
     }
 }
