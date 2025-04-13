deb0c9e681848afdbba04348a61765bf7136a764
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-13 12:43:06 +0200

    add return type

--- database/migrations/2023_03_02_000012_create_ts3_bot_logs_table.php
+++ database/migrations/2023_03_02_000012_create_ts3_bot_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('ts3_bot_logs', function (Blueprint $table) {
             $table->id();
