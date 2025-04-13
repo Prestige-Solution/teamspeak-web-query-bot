f7cac6d075451af672ff5533581a3293f5c32e6b
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-13 17:48:40 +0200

    remove unnecessary function add delete botlogs there can handle via schedule

--- app/Http/Controllers/admin/ResetStatsController.php
+++ app/Http/Controllers/admin/ResetStatsController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ts3Bot\ts3BotLog;
+use App\Models\ts3Bot\ts3ServerConfig;
 use App\Models\ts3BotWorkers\ts3BotWorkerPolice;
 
 class ResetStatsController extends Controller
 {
-    public function resetStats()
-    {
-        //TODO prepared for future implementations
-    }
-
     public function resetVPNQueryCountPerMinute(): void
     {
         ts3BotWorkerPolice::query()->update([
@@ -24,5 +21,20 @@ class ResetStatsController extends Controller
         ts3BotWorkerPolice::query()->update([
             'vpn_protection_query_per_day'=>0,
         ]);
+    }
+
+    public function deleteBotLogs(): void
+    {
+        $servers = ts3ServerConfig::query()->get(['id']);
+
+        foreach ($servers as $server) {
+            $count = ts3BotLog::query()->where(['server_id' => $server->id])->get()->count();
+            ts3BotLog::query()
+                ->where('server_id', $server->id)
+                ->orderByDesc('id')
+                ->take($count)
+                ->skip(100)
+                ->delete();
+        }
     }
 }
