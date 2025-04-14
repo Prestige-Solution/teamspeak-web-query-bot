034b5d90d1b7067e8216ba91c8f26de903dd9100
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-14 19:23:37 +0200

    change initial SetupCommand.php

--- app/Console/Commands/setup/SetupCommand.php
+++ app/Console/Commands/setup/SetupCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\setup;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class SetupCommand extends Command
 {
@@ -18,7 +19,7 @@ class SetupCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Setup Initial Server Configuration';
+    protected $description = 'Setup initial setup';
 
     /**
      * Execute the console command.
@@ -27,37 +28,52 @@ class SetupCommand extends Command
      */
     public function handle(): int
     {
-        $appURL = $this->ask('Enter your URL Address');
-        $dbHost = $this->ask('Enter your Database Connection (URL or IP)');
-        $dbName = $this->ask('Enter Database Name (Not the Username)');
-        $dbUsername = $this->ask('Enter Database Username');
-        $dbPassword = $this->secret('Enter Database Password');
+        $choiceGenerateAppKey = $this->choice('Generate app key?', ['yes', 'no'], 1);
+        $choiceDbCon = $this->choice('Choose your database',['mysql','pgsql'], 0);
+        $appURL = $this->ask('Enter your URL address');
+        $dbHost = $this->ask('Enter your database connection');
+        $dbName = $this->ask('Enter your database name');
+        $dbUsername = $this->ask('Enter your database user');
+        $dbPassword = $this->secret('Enter your database user password');
+
+        $dbCon = match ($choiceDbCon) {
+            'pgsql' => 'pgsql',
+            default => 'mysql',
+        };
+
+        if ($choiceGenerateAppKey === 'yes') {
+            Artisan::call('key:generate');
+        }
 
         $path = base_path('.env');
 
         if (file_exists($path)) {
             file_put_contents($path, str_replace(
-                'APP_URL='.$this->laravel['config']['url'], 'APP_URL='.$appURL, file_get_contents($path)
+                "APP_URL=", "APP_URL=".$appURL, file_get_contents($path)
             ));
 
             file_put_contents($path, str_replace(
-                'DB_HOST='.config('database.connections.mysql.host'), 'DB_HOST='.$dbHost, file_get_contents($path)
+                "DB_CONNECTION=", "DB_CONNECTION=".$dbCon, file_get_contents($path)
             ));
 
             file_put_contents($path, str_replace(
-                'DB_DATABASE='.config('database.connections.mysql.database'), 'DB_DATABASE='.$dbName, file_get_contents($path)
+                "DB_HOST=", "DB_HOST=".$dbHost, file_get_contents($path)
             ));
 
             file_put_contents($path, str_replace(
-                'DB_USERNAME='.config('database.connections.mysql.username'), 'DB_USERNAME='.$dbUsername, file_get_contents($path)
+                "DB_DATABASE=", "DB_DATABASE=".$dbName, file_get_contents($path)
             ));
 
             file_put_contents($path, str_replace(
-                'DB_PASSWORD='.config('database.connections.mysql.password'), 'DB_PASSWORD='.$dbPassword, file_get_contents($path)
+                "DB_USERNAME=", "DB_USERNAME=".$dbUsername, file_get_contents($path)
+            ));
+
+            file_put_contents($path, str_replace(
+                "DB_PASSWORD=", "DB_PASSWORD=".$dbPassword, file_get_contents($path)
             ));
         }
 
-        $this->info('Server Configuration Setup Completed');
+        $this->info('Setup finished');
 
         return self::SUCCESS;
     }
