f12bfddd1e1e76c84eac292689d19408fdd68583
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-15 20:21:42 +0200

    extend footer

--- resources/views/navs/main-footer.blade.php
+++ resources/views/navs/main-footer.blade.php
@@ -2,12 +2,17 @@
     <footer class="footer py-3 bg-dark">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                <span class="text-muted d-grid justify-content-center">
-                    <span id="current_version">
-                        <a class="link-secondary text-decoration-none" href="https://www.prestige-solutions.de/" target="_blank" rel="nofollow">&copy; Prestige Solutions {{ \Illuminate\Support\Carbon::now()->format('Y') }} </a>
-                    </span>
-                </span>
+                <div class="col-lg-12 d-flex justify-content-between">
+                    <div>
+                        <span class="text-muted">
+                            <a class="link-secondary text-decoration-none" href="https://github.com/Prestige-Solution" target="_blank" rel="nofollow">&copy; {{ \Illuminate\Support\Carbon::now()->format('Y') }} | Developed by Prestige Solutions with &#x2764; </a>
+                        </span>
+                    </div>
+                    <div>
+                        <a class="text-decoration-none text-danger me-2" href="https://laravel.com/" target="_blank" rel="nofollow"><i class="fa-brands fa-laravel fa-2x"></i></a>
+                        <a class="text-decoration-none text-white me-2" href="https://github.com/Prestige-Solution" target="_blank" rel="nofollow"><i class="fa-brands fa-github fa-2x"></i></a>
+                        <a class="text-decoration-none text-white" href="https://getbootstrap.com/" target="_blank" rel="nofollow"><i class="fa-brands fa-bootstrap fa-2x"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
