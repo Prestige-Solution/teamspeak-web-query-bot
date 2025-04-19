3ccac4035506ae28cfac9fae21fa4dd8b79d5b7d
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-04-19 10:35:24 +0200

    change to a standard setup routine

--- app/Console/Commands/setup/SetupCommand.php
+++ app/Console/Commands/setup/SetupCommand.php
@@ -19,7 +19,7 @@ class SetupCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Setup initial setup';
+    protected $description = 'Setup application';
 
     /**
      * Execute the console command.
@@ -28,52 +28,13 @@ class SetupCommand extends Command
      */
     public function handle(): int
     {
-        $choiceGenerateAppKey = $this->choice('Generate app key?', ['yes', 'no'], 1);
-        $choiceDbCon = $this->choice('Choose your database', ['mysql', 'pgsql'], 0);
-        $appURL = $this->ask('Enter your URL address');
-        $dbHost = $this->ask('Enter your database connection');
-        $dbName = $this->ask('Enter your database name');
-        $dbUsername = $this->ask('Enter your database user');
-        $dbPassword = $this->secret('Enter your database user password');
-
-        $dbCon = match ($choiceDbCon) {
-            'pgsql' => 'pgsql',
-            default => 'mysql',
-        };
-
-        if ($choiceGenerateAppKey === 'yes') {
-            Artisan::call('key:generate');
-        }
-
-        $path = base_path('.env');
-
-        if (file_exists($path)) {
-            file_put_contents($path, str_replace(
-                'APP_URL=', 'APP_URL='.$appURL, file_get_contents($path)
-            ));
-
-            file_put_contents($path, str_replace(
-                'DB_CONNECTION=', 'DB_CONNECTION='.$dbCon, file_get_contents($path)
-            ));
-
-            file_put_contents($path, str_replace(
-                'DB_HOST=', 'DB_HOST='.$dbHost, file_get_contents($path)
-            ));
-
-            file_put_contents($path, str_replace(
-                'DB_DATABASE=', 'DB_DATABASE='.$dbName, file_get_contents($path)
-            ));
-
-            file_put_contents($path, str_replace(
-                'DB_USERNAME=', 'DB_USERNAME='.$dbUsername, file_get_contents($path)
-            ));
-
-            file_put_contents($path, str_replace(
-                'DB_PASSWORD=', 'DB_PASSWORD='.$dbPassword, file_get_contents($path)
-            ));
-        }
-
-        $this->info('Setup finished');
+        Artisan::call('storage:link');
+        Artisan::call('migrate');
+        Artisan::call('migrate:status');
+        Artisan::call('db:seed');
+        Artisan::call('cache:clear');
+        Artisan::call('optimize');
+        Artisan::call('view:cache');
 
         return self::SUCCESS;
     }
