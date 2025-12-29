315f32de1fc08564edea60d8bebf2f8da2c2f9ba
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-12-29 15:03:47 +0100

    update migration blade with more detailed steps

--- resources/views/backend/utils/migration/migrate.blade.php
+++ resources/views/backend/utils/migration/migrate.blade.php
@@ -8,20 +8,33 @@
 <div class="container mt-3">
     <div class="row mb-2">
         <div class="col-lg-8">
-            <h2 class="fs-3 fw-bold">Teamspeak Server Migration Tool</h2>
+            <h2 class="fs-3 fw-bold">Migration Tool</h2>
         </div>
     </div>
     <hr>
     <div class="row">
-        <div class="col-lg-12">
-            <p>
-                This tool will help you to migrate data from one server to another.
-            </p>
-            <ol>
-                <li>Setup your servers with query <span class="fw-bold">admin user</span> account</li>
-                <li>Select source server</li>
-                <li>Select target server</li>
-            </ol>
+        <div class="col-lg-6 d-flex">
+            <div class="card flex-fill">
+                <div class="card-body">
+                    <h5 class="card-title fw-bold">Steps</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">1. Setup your servers with <span class="fw-bold">query admin user</span> account</li>
+                        <li class="list-group-item">2. Select source server</li>
+                        <li class="list-group-item">3. Select target server</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 d-flex">
+            <div class="card flex-fill">
+                <div class="card-body">
+                    <h5 class="card-title fw-bold">Notes</h5>
+                    <p class="card-text">
+                        If you are using a Teamspeak 3 server, make sure you have configured and prepared it correctly. Instructions can be found here
+                        <a href="https://github.com/Prestige-Solution/ts-x-php-framework/blob/main/doc/docker/make-ts3-ssh-compatible.md#setup-a-ssh_rsa_host_key" target="_blank">Setup a ssh_rsa_host_key</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
     <hr>
@@ -29,6 +42,11 @@
     @include('form-components.successCustom')
     <form action="{{route('migration.start.migration')}}" method="post">
         @csrf
+        <div class="row mb-2">
+            <div class="col-lg-12">
+                <h2 class="fs-3 fw-bold">Settings</h2>
+            </div>
+        </div>
         <div class="row">
             <div class="col-lg-5">
                 <label class="col-form-label fw-bold" for="source_server_id">Source Server</label>
