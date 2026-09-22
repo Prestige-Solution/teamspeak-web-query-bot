d8b481febb7d8b36d773064d4d666fffdc1876c3
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2026-09-22 15:41:00 +0200

    fix: remove import statement for Auth

--- app/Http/Controllers/sys/MigrationController.php
+++ app/Http/Controllers/sys/MigrationController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Migration\StartMigrationRequest;
 use App\Jobs\tsMigrationQueue;
 use App\Models\tsBot\tsServerConfig;
-use Illuminate\Support\Facades\Auth;
 
 class MigrationController extends Controller
 {
